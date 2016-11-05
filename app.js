a1939cf5621efccb8eac4595ea8463b7d9584aaf
Asheni De Silva <asheni_silva@accelerite.com>
2016-11-05 12:11:41 -0500

    ws1

--- app.js
+++ app.js
@@ -15,13 +15,13 @@ var express = require('express');
 var cfenv = require('cfenv');
 
 // create a new express server
-//var app = express();
+var app = express();
 //
 //// serve the files out of ./public as our main files
-//app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/public'));
 //
 //// get the app environment from Cloud Foundry
-//var appEnv = cfenv.getAppEnv();
+var appEnv = cfenv.getAppEnv();
 //
 ///*// start server on the specified port and binding host
 //app.listen(appEnv.port, '0.0.0.0', function() {
@@ -64,8 +64,8 @@ var cfenv = require('cfenv');
 
 
 
-//app.listen(appEnv.port, '0.0.0.0', function() {
-//  // print a message when the server starts listening
-//  console.log("server starting on " + appEnv.url);
-//});
+app.listen(appEnv.port, '0.0.0.0', function() {
+  // print a message when the server starts listening
+  console.log("server starting on " + appEnv.url);
+});
 
